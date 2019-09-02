e65d2f7538dc37c4a98927611aca4e63f43ac111
Anton Zimin <anton_zimin@epam.com>
2019-09-02 18:27:51 +0300

    hometask selenium advanced

--- spec.js
+++ spec.js
@@ -1,9 +1,31 @@
+var webDriver = require('selenium-webdriver');
+var driver = new webDriver.Builder()
+    .usingServer('http://localhost:4444/wd/hub')
+    .withCapabilities(webDriver.Capabilities.firefox())
+    .build();
+
 describe('protractor test', function() {
-    it('should have a title', function() {
-      browser.get('https://www.zagat.com/');
-      element(by.xpath("//a[contains(@class, 'zgt-latest-content-more')]")).click();
-      element(by.xpath("//span[contains(@class, 'zgt-media-card-title-text') and text()='The 50 Best Restaurants in Washington DC']")).click();
-      element(by.xpath("//span[contains(@class, 'zgt-basic-facts-title-text') and text()='Himitsu']")).click();
-        expect(browser.getTitle()).toEqual('The 50 Best Restaurants in Washington DC - Zagat');
+
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    return browser.manage().window().maximize();
+    
+    });
+
+    it('should have a title', async function() {
+        
+      await browser.get('https://www.apc.com/shop/us/en/tools/ups_selector/');
+      var flowSelector = element(by.xpath("//label[contains(@class, 'FlowSelector') and text()='Home, Home Office and Small Business']"));
+      await browser.actions().mouseMove(flowSelector, {x: 0, y: 0}).mouseDown().mouseUp().perform();
+      var configureButton = element(by.xpath("//button[contains(@class, 'FlowSelector_btnFlowSelection') and child::span[text()='Configure by Load']]"));
+      await browser.executeScript("arguments[0].setAttribute('style', 'background: yellow;')", configureButton);
+      browser.sleep(4000);
+      await browser.executeScript("arguments[0].click()", configureButton);
+      browser.sleep(3000);
+      
+      await browser.element(by.xpath("//input[contains(@class, 'CustomTextField_seCustomInput')]")).sendKeys('600');
+      var continueButton = element(by.xpath("//a[contains(@class, 'SeButton_primary') and child::span[text()='Continue']]"));
+      await browser.actions().mouseMove(continueButton, {x: 0, y: 0}).mouseDown().mouseUp().perform();
+      expect(browser.getTitle()).toEqual('APC UPS Selector/Calculator - Find the Correct Battery Backup');
     });
 });
